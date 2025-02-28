8c51a2f01726490b169f3302040844d9121d0fc5
Toth Patrik <toth.patrik@BOLYAI.LOCAL>
2025-02-28 08:46:03 +0100

    orai munka

--- HOF/kitero.js
+++ HOF/kitero.js
@@ -20,9 +20,19 @@ const fv3 = (operator) => {
             return a-b;
         }
     }
+    if(operator === '*2'){
+        const multi = 2;
+        return(a,b) => {
+            return multi * (a+b)
+        }
+    }
 }
 const fv4 = fv3('-')
 console.log(fv4(5,7))
 
+const res3 = fv2(5,7,fv3('-'))
+console.log(res3)
 
+const res4 = fv2(5,7,fv3('*2'))
+console.log(res4)
 
